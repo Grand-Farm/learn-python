453a8a55f49f4f08751ce214fff4b7fafaba56eb
Philippe <kamokamophilippe13@gmail.com>
2022-09-28 08:21:22 -0500

    for lopp and while loop

---
+++ loop.py
@@ -0,0 +1,13 @@
+number = 0
+while number < 100:
+    number += 2
+    print(number)
+
+numbers = 100
+
+for number in range(1, numbers + 1):
+    if(number % 2 == 0):
+
+      print(number)
+
+# event number from to 100 with for loop and while loop
